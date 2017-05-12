9ce5bcd60239870fe5b890341e88286d5a4422bb
prgdanny90 <prgdanny90@gmail.com>
2017-05-13 04:47:44 +0900

    오늘은 색깔로 만족
    ㅇ

--- eclass_assignment_filter.js
+++ eclass_assignment_filter.js
@@ -1,36 +1,34 @@
 ﻿// Eclass-Assignment-Filter
 
- (function () {
-   var btn = document.createElement("BUTTON");
-   var t = document.createTextNode("미제출 보기");
-   btn.appendChild(t);
-   btn.onclick = function(){
-	   var term = "미제출";
-	   var tables = document.getElementsByClassName('list-table');
-	   var table = tables[0];
-	   var tr = table.getElementsByTagName('TR');
-	   for (var i = tr.length - 1; i >= 1; i--) {
-	       var td = tr[i].getElementsByTagName('TD');
-		   if (td[4].innerText == term) {
-		       //alert(td[2].innerText);
-		       //tr[i].style.backgroundColor = "yellow";
-		       //td[4].innerHTML = "찾았다.";
-		   }
-	       else
-		   {
-               table.parentNode.removeChild(tr[i]);
-		       //for (var j = 1; j < td.length; j++) {
-		       //    td[j].style.color = "white";
-		       //}
-		   }
-		   //else
-		   //{
-		   //    alert(i + "번째 는 완료된 항목" + td[2]);
-		   //}
-	   }
-	   };
-   document.body.appendChild(btn);
- })();
+(function () {
+    var btn = document.createElement("BUTTON");
+    var t = document.createTextNode("미제출 보기");
+    btn.appendChild(t);
+    btn.onclick = function () {
+        var term = "미제출";
+        var tables = document.getElementsByClassName('list-table');
+        var table = tables[0];
+        var tr = table.getElementsByTagName('TR');
+        for (var i = 1; i < tr.length; i++) {
+            var td = tr[i].getElementsByTagName('TD');
+            if (td[4].innerText == term) {
+                alert(td[2].innerText);
+                tr[i].style.backgroundColor = "yellow";
+                //td[4].innerHTML = "찾았다.";
+            }
+            else {
+                for (var j = 1; j < td.length; j++) {
+                    td[j].style.color = "white";
+                }
+            }
+            //else
+            //{
+            //    alert(i + "번째 는 완료된 항목" + td[2]);
+            //}
+        }
+    };
+    document.body.appendChild(btn);
+})();
 
 //(function () {
 //  var term = "미제출";
